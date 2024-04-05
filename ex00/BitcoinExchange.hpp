bfb0d44f203af16a7c92ae2cd0f7e2457766bcb8
nschutz <nschutz@student.42wolfsburg.de>
2024-04-05 15:41:22 +0200

    CPP09 feat: ex00 first functions

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -10,4 +10,12 @@ public:
     BitcoinExchange(BitcoinExchange const &src);
     BitcoinExchange &operator=(BitcoinExchange const &src);
     ~BitcoinExchange();
+
+    void check_input(std::string const &line);
+    void get_date(std::string const &line);
+
+private:
+    std::string date;
+    std::map<std::string> db;
+
 };
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,1 +1,32 @@
 #include "BitcoinExchange.hpp"
+
+int error_check(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Error: Incorrect number of arguments" << std::endl;
+        return 1;
+    }
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        std::cerr << "Error: File does not exist" << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (error_check(argc, argv))
+        return 1;
+    BitcoinExchange exchange;
+    std::string line;
+    while (std::getline(fd, line)) {
+        try{
+            exchange.check_input(line);
+            exchange.get_date(line);
+        } catch (std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return 1;
+        }
+    }
+    close(fd);
+}
