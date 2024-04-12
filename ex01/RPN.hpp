e780940153e20f5fad26d60416807c9232c4add8
nschutz <nschutz@student.42wolfsburg.de>
2024-04-12 10:55:20 +0200

    CPP 09 feat: ex01  first input for files

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -4,3 +4,18 @@
 #include <stack>
 #include <algorithm>
 
+class RPN
+{
+public:
+    RPN();
+    RPN(RPN const &src);
+    RPN &operator=(RPN const &src);
+    ~RPN();
+
+    void calculate(std::string const &line);
+    void print_result() const;
+
+private:
+    std::stack<int> stack;
+    int result;
+};
